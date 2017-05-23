90efed0328a0692753d675daf28cac39320a21cc
shrutiburnwal <shruti.nitdgp@gmail.com>
2017-05-23 11:57:39 +0530

    Create mario.c

---
+++ Problemset1/mario.c
@@ -0,0 +1,34 @@
+https://ide.cs50.io/shrucodeti/cs50#openfile-README.md //link for reference
+
+
+
+#include<stdio.h>
+#include<cs50.h>
+int main(void)
+{
+  int c=1,j,height;
+  printf("Height:");
+  height=get_int();
+  while(height<0||height>23)
+  {
+    printf("Retry:");
+    height=get_int();  
+  }
+  while(c<=height)
+  {
+      j=0;
+      while(j<height-c)
+      {
+          printf(" ");
+          j++;
+      }
+      while(j<=height)
+      {
+          printf("#");
+          j++;
+      }
+  c++;
+  printf("\n");
+  }  
+}        
+         
