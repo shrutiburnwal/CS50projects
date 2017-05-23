06e1d33144c82f74f021c11d6866b7bac4872bbd
shrutiburnwal <shruti.nitdgp@gmail.com>
2017-05-23 12:02:29 +0530

    Update hello.c

--- Problemset1/hello.c
+++ Problemset1/hello.c
@@ -1,5 +1,5 @@
-https://ide.cs50.io/shrucodeti/cs50#openfile-README.md //link for CS50 ide for reference
-
+//https://ide.cs50.io/shrucodeti/cs50#openfile-README.md //link for CS50 ide for reference
+//https://cs50.me/submissions/cs50/2017/x/hello //LINK OF SUBMISSION
 
 #include <stdio.h>
 
